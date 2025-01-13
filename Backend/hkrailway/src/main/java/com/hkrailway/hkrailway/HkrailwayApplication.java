2f1b4234a86bb8ad7f29d1f96a3463054a17f9a4
hung0125 <hungyh0125@gmail.com>
2025-01-14 00:05:40 +0800

    Initial Structure

---
+++ Backend/hkrailway/src/main/java/com/hkrailway/hkrailway/HkrailwayApplication.java
@@ -0,0 +1,13 @@
+package com.hkrailway.hkrailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HkrailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HkrailwayApplication.class, args);
+	}
+
+}
---
+++ Backend/hkrailway/src/test/java/com/hkrailway/hkrailway/HkrailwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkrailway.hkrailway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkrailwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
